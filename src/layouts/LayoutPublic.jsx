f96fd82cb92577a5242297ba9d8725cf166754bd
avilrod3004 <avilrod3004@g.educaand.es>
2024-11-18 10:23:12 +0100

    añadir boton de inicio de sesion y registro

--- src/layouts/LayoutPublic.jsx
+++ src/layouts/LayoutPublic.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import Navbar from "./components/Navbar.jsx";
 import {Outlet} from "react-router-dom";
+import Footer from "./components/Footer.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Button from "../components/Button.jsx";
 
 const LayoutPublic = () => {
     return (
         <>
-            <header>
+            <header style={{ border: `1px solid black` }}>
                 <h1>GoodFood</h1>
                 <Navbar/>
+                <Button texto={"Login"}></Button>
+                <Button texto={"Sign up"}></Button>
             </header>
 
             <Outlet/>
 
-            <footer>
-                soy un footer
-            </footer>
+            <Footer/>
         </>
     );
 };
