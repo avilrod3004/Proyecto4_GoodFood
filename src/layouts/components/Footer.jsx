2cf2a7f093ad3bafe77b6924c0b10b4aef883fc4
avilrod3004 <avilrod3004@g.educaand.es>
2024-11-18 10:23:59 +0100

    footer con enlaces

---
+++ src/layouts/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <footer>
+            <nav>
+                <a href="#">c 2024 GoodFood</a>
+                <a href="#">Terms of Service</a>
+                <a href="#">Privacy Policy</a>
+                <a href="#">Manege Privacy Preferences</a>
+            </nav>
+        </footer>
+    );
+};
+
+export default Footer;
