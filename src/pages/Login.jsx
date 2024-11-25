0467147d728ba4ebdba6efed3bd973aad8f2158d
avilrod3004 <avilrod3004@g.educaand.es>
2024-11-25 10:56:46 +0100

    inputs formulario del login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <>
+            <img src="/img_login.jpeg" alt="Img login"/>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Login</button>
+            </form>
+        </>
+    );
+};
+
+export default Login;
