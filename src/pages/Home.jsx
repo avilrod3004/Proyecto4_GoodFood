928cc559b1aa68baebee5a02b94e640392aaf461
avilrod3004 <avilrod3004@g.educaand.es>
2024-11-25 10:55:15 +0100

    texto entrada

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Button from "../components/Button.jsx";
+// import "../../public/img_home.jpeg"
 
 const Home = () => {
     return (
         <>
-            Home
+            <h1>Looking for new recipes</h1>
+            <h2>Bored of always eating the same thing?</h2>
+            <img src="/img_home.jpeg" alt="Cocina"/>
+            <Button texto="What would you like to cook?"></Button>
+
+        {/* llamada api para mostrar 20 recetas */}
         </>
     );
 };
