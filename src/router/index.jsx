6c3af74a156a3a61cd80c219eb873851134a249e
avilrod3004 <avilrod3004@g.educaand.es>
2024-11-27 09:54:31 +0100

    correccion ruta

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -55,7 +55,7 @@ export const router = createBrowserRouter([
                         ]
                     },
                     {
-                        path: "lunchdinner",
+                        path: "/lunchdinner",
                         element: <LayoutPrivate/>,
                         children: [
                             {
