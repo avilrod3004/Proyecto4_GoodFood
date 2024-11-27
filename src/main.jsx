b9fd1fbb1af559528a591e1d125c4a0d1a8ccb25
avilrod3004 <avilrod3004@g.educaand.es>
2024-11-27 13:53:33 +0100

    lazy load

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import {StrictMode, Suspense} from 'react'
 import { createRoot } from 'react-dom/client'
 import {RouterProvider} from "react-router-dom";
 import {router} from "./router/index.jsx";
@@ -6,8 +6,10 @@ import UserProvider from "./context/UserContext.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UserProvider>
-        <RouterProvider router={router}/>
-    </UserProvider>
+    <Suspense fallback={<div>Loading...</div>}>
+        <UserProvider>
+            <RouterProvider router={router}/>
+        </UserProvider>
+    </Suspense>
   </StrictMode>
 )
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,15 +1,17 @@
 import {createBrowserRouter} from "react-router-dom";
 import LayoutPublic from "../layouts/LayoutPublic.jsx";
-import NotFound from "../pages/NotFound.jsx";
+import LayoutPrivate from "../layouts/LayoutPrivate.jsx";
 import Home from "../pages/Home.jsx";
-import QuickRecipes from "../pages/QuickRecipes.jsx";
-import Breakfast from "../pages/Breakfast.jsx";
-import LunchDinner from "../pages/LunchDinner.jsx";
 import ContactUs from "../pages/ContactUs.jsx";
 import Login from "../pages/Login.jsx";
 import Register from "../pages/Register.jsx";
-import UserProfile from "../pages/UserProfile.jsx";
-import LayoutPrivate from "../layouts/LayoutPrivate.jsx";
+import React from "react";
+
+const QuickRecipes = React.lazy(() => import("../pages/QuickRecipes"));
+const Breakfast = React.lazy(() => import("../pages/Breakfast"));
+const LunchDinner = React.lazy(() => import("../pages/LunchDinner"));
+const UserProfile = React.lazy(() => import("../pages/UserProfile"));
+const NotFound = React.lazy(() => import("../pages/NotFound"));
 
 export const router = createBrowserRouter([
 
