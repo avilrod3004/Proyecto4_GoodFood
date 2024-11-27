71e0315aed818d56b48734b718fb96d8f9f4890a
avilrod3004 <avilrod3004@g.educaand.es>
2024-11-27 20:33:55 +0100

    implementacion basica de paginacion

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Button from "../components/Button.jsx";
 // import "../../public/img_home.jpeg"
 import "../sass/main.scss"
@@ -7,6 +7,8 @@ const Home = () => {
     // Estados
     const [recipes, setRecipes] = React.useState([]);
     const [loading, setLoading] = React.useState(true);
+    const [currentPage, setCurrentPage] = useState(1); // Página actual
+    const recipesPerPage = 3; // Número de recetas por página
 
     async function getRandomRecipes() {
         try {
@@ -25,6 +27,25 @@ const Home = () => {
         getRandomRecipes();
     }, [])
 
+    // Lógica para obtener las recetas de la página actual
+    const indexOfLastRecipe = currentPage * recipesPerPage;
+    const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage;
+    const currentRecipes = recipes.slice(indexOfFirstRecipe, indexOfLastRecipe);
+
+    // Cambiar página
+    const nextPage = () => {
+        if (currentPage < Math.ceil(recipes.length / recipesPerPage)) {
+            setCurrentPage((prevPage) => prevPage + 1);
+        }
+    };
+
+    const prevPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage((prevPage) => prevPage - 1);
+        }
+    };
+
+
 
     return (
         <>
@@ -35,11 +56,32 @@ const Home = () => {
 
             {loading && <p>Loading...</p>}
             {/* llamada api para mostrar 20 recetas */}
-            <ul>
-                {recipes.map((recipe, index) => (
-                    <li key={index}>{recipe.recipe.label} || {recipe.recipe.mealType} || {recipe.recipe.cuisineType}</li>
-                ))}
-            </ul>
+            {!loading && (
+                <>
+                    <ul>
+                        {currentRecipes.map((recipe, index) => (
+                            <li key={index}>
+                                {recipe.recipe.label} || {recipe.recipe.mealType} || {recipe.recipe.cuisineType}
+                            </li>
+                        ))}
+                    </ul>
+                    {/* Botones de navegación */}
+                    <div>
+                        <button onClick={prevPage} disabled={currentPage === 1}>
+                            Previous
+                        </button>
+                        <span>
+                            Page {currentPage} of {Math.ceil(recipes.length / recipesPerPage)}
+                        </span>
+                        <button
+                            onClick={nextPage}
+                            disabled={currentPage === Math.ceil(recipes.length / recipesPerPage)}
+                        >
+                            Next
+                        </button>
+                    </div>
+                </>
+            )}
         </>
     );
 };
