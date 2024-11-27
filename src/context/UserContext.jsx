dea271bdb40cc0663f1fb0786686c60abe445f3b
avilrod3004 <avilrod3004@g.educaand.es>
2024-11-27 09:55:33 +0100

    observador para detectar usuarios logueados

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,9 +1,24 @@
-import {createContext, useState} from "react";
+import {createContext, useEffect, useState} from "react";
+import { onAuthStateChanged } from 'firebase/auth'
+import {auth} from "../config/Firebase.jsx";
 
 export const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
     const [user, setUser] = useState(false);
+
+    // Observador para detectar usuarios logueados
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            }
+        })
+    }, [])
+
+    if (user === null) {
+        return <p>Loading...</p>;
+    }
 
     return (
         <UserContext.Provider value={{ user, setUser }}>
