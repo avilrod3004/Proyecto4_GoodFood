d2363652b8ede802af68b83aee691dda29c56393
avilrod3004 <avilrod3004@g.educaand.es>
2024-11-29 19:19:47 +0100

    redirigir a la pagina para consultar los datos de la receta

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom";
 
 /**
  * Tarjeta vertical que muestra información básica de las recetas devueltas por la API
@@ -8,14 +9,22 @@ import React from 'react';
  * @param cuisineType Tipo de cocina
  * @returns {Element} Tarjeta
  */
-const Card = ({image, title, mealType, cuisineType}) => {
+const Card = ({id, image, title, mealType, cuisineType}) => {
+    const navigate = useNavigate();
+
+    const consultarReceta = () => {
+        localStorage.setItem("receta", id);
+        navigate(`/receta/${id}`);
+    }
+
     return (
-        <article style={{ border: `1px solid black`}}>
+        <article style={{ border: `1px solid black`}} onClick={() => {consultarReceta()}}>
             <img src={image} alt={title}/>
             <h1>{title}</h1>
             <ul>
                 <li>{mealType}</li>
                 <li>{cuisineType}</li>
+                <li>{id}</li>
             </ul>
 
         </article>
