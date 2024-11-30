767146c08c218fbb06f2056bc818ffd9e4da5a94
avilrod3004 <avilrod3004@g.educaand.es>
2024-11-30 19:02:32 +0100

    llamada a la api aplicando filtros :)

--- src/pages/QuickRecipes.jsx
+++ src/pages/QuickRecipes.jsx
@@ -1,7 +1,17 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import FilterRecipes from "../components/FilterRecipes.jsx";
+import Card from "../components/Card.jsx";
 
 const QuickRecipes = () => {
+    // Datos API de recetas
+    const api_data = {
+        id: import.meta.env.VITE_RECIPES_API_ID,
+        key: import.meta.env.VITE_RECIPES_API_KEY,
+    }
+
+    const url = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${api_data.id}&app_key=${api_data.key}`
+    let urlFilters = url;
+
     // Estados iniciales
     const filtersInitialValues = {
         q: "",
@@ -19,6 +29,57 @@ const QuickRecipes = () => {
     // Estados
     const [filters, setFilters] = useState(filtersInitialValues)
     const [filterErrors, setFilterErrors] = useState(errorFiltersInitial)
+    const [recipes, setRecipes] = React.useState([]);
+    const [loading, setLoading] = React.useState(true);
+
+    const addFilters = () => {
+        urlFilters = url
+
+        if (filters.q.trim() !== "") {
+            urlFilters = urlFilters + "&q=" + filters.q;
+        }
+
+        if (filters.mealTypes.length > 0) {
+            filters.mealTypes.forEach(mealType => {
+                urlFilters = urlFilters + "&mealType=" + mealType.toLowerCase();
+            })
+        }
+
+        if (filters.cuisineTypes.length > 0) {
+            filters.cuisineTypes.forEach(cuisineType => {
+                urlFilters = urlFilters + "&cuisineType=" + cuisineType.toLowerCase();
+            })
+        }
+
+        if (filters.health.length > 0) {
+            filters.health.forEach(health => {
+                urlFilters = urlFilters + "&health=" + health.toLowerCase();
+            })
+        }
+
+        if (filters.maxTime.trim() !== "") {
+            urlFilters = urlFilters + "&time=" + filters.maxTime;
+        }
+    }
+
+    async function getRecipes() {
+        try {
+            const response = await fetch(urlFilters)
+            const data = await response.json()
+            setRecipes(data.hits || [])
+            setLoading(false)
+        } catch (error) {
+            setLoading(false)
+        }
+    }
+
+    const getRecipeId = (recipeUri) => recipeUri.split('recipe_')[1]
+
+    useEffect(() => {
+        addFilters()
+        console.log(urlFilters)
+        getRecipes()
+    }, [filters])
 
     return (
         <>
@@ -30,6 +91,24 @@ const QuickRecipes = () => {
                 filterErrors={filterErrors}
                 setFilterErrors={setFilterErrors}
             />
+
+            {loading && <p>Loading...</p>}
+
+            {!loading && (
+                <>
+                    <section>
+                        {recipes.map((recipe, index) => (
+                            <Card
+                                key={index}
+                                id={getRecipeId(recipe.recipe.uri)}
+                                image={recipe.recipe.images.THUMBNAIL.url}
+                                title={recipe.recipe.label}
+                                mealType={recipe.recipe.mealType}
+                                cuisineType={recipe.recipe.cuisineType}/>
+                        ))}
+                    </section>
+                </>
+            )}
         </>
     );
 };
