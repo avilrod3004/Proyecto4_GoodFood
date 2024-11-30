2ef7827fe922a5854885dd086a0662416e7b0c1d
avilrod3004 <avilrod3004@g.educaand.es>
2024-11-30 22:52:19 +0100

    base de datos con indexedDB, funciones para añadir y consultar usuarios

---
+++ src/config/IndexedDB.jsx
@@ -0,0 +1,48 @@
+const request = indexedDB.open("GoodFoodDB", 1);
+let db;
+
+request.onerror = event => {
+    console.error("ERROR", event.target.errorCode);
+}
+
+request.onsuccess = event => {
+    db = event.target.result;
+}
+
+request.onupgradeneeded = event => {
+    db = event.target.result;
+
+    if (!db.objectStoreNames.contains("users")) {
+        const objectStore = db.createObjectStore("users", {keyPath: "id", autoIncrement: true});
+        objectStore.createIndex("email", "email", {unique: true});
+    }
+}
+
+const addUser = (user) => {
+    const transaction = db.transaction(["users"], "readwrite");
+    const objectStore = transaction.objectStore("users");
+    const request = objectStore.put(user);
+
+    request.onerror = event => {
+        console.error("ERROR", event.target.errorCode);
+    }
+
+    request.onsuccess = event => {
+        console.log("SUCCESS", event.target.result);
+    }
+}
+
+const getUser = (email) => {
+    const transaction = db.transaction(["users"], "readonly");
+    const objectStore = transaction.objectStore("users");
+    const index = objectStore.index("email");
+    const request = index.get(email);
+
+    request.onerror = event => {
+        console.error("ERROR", event.target.errorCode);
+    }
+
+    request.onsuccess = event => {
+        console.log("SUCCESS", event.target.result);
+    }
+}
