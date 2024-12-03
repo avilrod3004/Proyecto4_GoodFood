3de05a63de7eb7ab94cbb095115afbb80e60d6b4
avilrod3004 <avilrod3004@g.educaand.es>
2024-12-03 14:12:23 +0100

    cambios: refactorizacion y comentarios

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -20,6 +20,7 @@ const UserProfile = () => {
         favoriteRecipes: []
     }
 
+    // Estados
     const {user, setUser} = useContext(UserContext);
     const [userData, setUserData] = useState(userDataInitial);
     const [cardSize, setCardSize] = useState("small");
@@ -27,6 +28,7 @@ const UserProfile = () => {
     const [error, setError] = useState("");
 
 
+    // Obtener los datos del usuario de la base de datos
     const fetchUserData = async (uid) => {
         try {
             const data = await getUserData(uid);
@@ -41,7 +43,6 @@ const UserProfile = () => {
             });
 
             setLoading(false);
-            console.log(data);
         } catch (error) {
             setLoading(false);
             setError(error);
@@ -49,72 +50,71 @@ const UserProfile = () => {
         }
     };
 
+    // Cuando el usuario logueado se haya cargado
     useEffect(() => {
         if (user?.uid) {
             fetchUserData(user.uid);
         }
     }, [])
 
+    if (loading) return <p>Cargando...</p>;
+    if (error) return <p>Error: {error}</p>;
+
     return (
         <>
-            {loading && <p>Loading...</p>}
-            {error !== "" && <p>Error: {error}</p>}
+            <aside>
+                <h1>{userData.userName || "No hay usuario"}</h1>
+                <img src={userData.picture || ""} alt="Foto de perfil"/>
+                <p>{userData.biography || "No hay biografía"}</p>
+                <h2>Website</h2>
+                <p>
+                    <a href={userData.website || "#"} target="_blank">
+                        {userData.website || "No hay website"}
+                    </a>
+                </p>
+                <h2>Social accounts</h2>
+                <ul>
+                    <li>{userData.socialAccount1 || "No hay social account"}</li>
+                    <li>{userData.socialAccount2 || "No hay social account"}</li>
+                    <li>{userData.socialAccount3 || "No hay social account"}</li>
+                </ul>
+            </aside>
 
-            {
-                !loading && error === "" && (
-                    <>
-                        <aside>
-                            <h1>{userData ? userData.userName : "No hay usuario"}</h1>
-                            <img src={userData && userData.picture} alt="Foto de perfil"/>
-                            <p>{userData ? userData.biography : "No hay biografia"}</p>
-                            <h2>Website</h2>
-                            <p><a href="">{userData ? userData.website : "No hay website"}</a></p>
-                            <h2>Social accounts</h2>
-                            <ul>
-                                <li>{userData ? userData.socialAccount1 : "No hay social account"}</li>
-                                <li>{userData ? userData.socialAccount2 : "No hay social account"}</li>
-                                <li>{userData ? userData.socialAccount3 : "No hay social account"}</li>
-                            </ul>
-                        </aside>
+            <section>
+                <h1>My favorite recipes</h1>
+                <header>
+                    <button onClick={() => {
+                        cardSize === "small" ? setCardSize("big") : setCardSize("small")
+                    }}>Change card size
+                    </button>
+                </header>
 
-                        <section>
-                            <h1>My favorite recipes</h1>
-                            <header>
-                                <button onClick={() => {
-                                    cardSize === "small" ? setCardSize("big") : setCardSize("small")
-                                }}>Change card size
-                                </button>
-                            </header>
-
-                            {
-                                userData.favoriteRecipes.map((recipe) => (
-                                    cardSize === "small" ? (
-                                        <SmallCard
-                                            key={recipe.id}
-                                            id={recipe.id}
-                                            image={recipe.image}
-                                            title={recipe.title}
-                                            mealType={recipe.mealType}
-                                            cuisineType={recipe.cuisineType}
-                                        />
-                                    ) : (
-                                        <BigCard
-                                            key={recipe.id}
-                                            id={recipe.id}
-                                            image={recipe.image}
-                                            title={recipe.title}
-                                            mealType={recipe.mealType}
-                                            cuisineType={recipe.cuisineType}
-                                            healthLabels={recipe.healthLabels}
-                                            totalTime={recipe.totalTime}
-                                        />
-                                    )
-                                ))
-                            }
-                        </section>
-                    </>
-                )
-            }
+                {
+                    userData.favoriteRecipes.map((recipe) => (
+                        cardSize === "small" ? (
+                            <SmallCard
+                                key={recipe.id}
+                                id={recipe.id}
+                                image={recipe.image}
+                                title={recipe.title}
+                                mealType={recipe.mealType}
+                                cuisineType={recipe.cuisineType}
+                            />
+                        ) : (
+                            <BigCard
+                                key={recipe.id}
+                                id={recipe.id}
+                                image={recipe.image}
+                                title={recipe.title}
+                                mealType={recipe.mealType}
+                                cuisineType={recipe.cuisineType}
+                                healthLabels={recipe.healthLabels}
+                                totalTime={recipe.totalTime}
+                            />
+                        )
+                    ))
+                }
+            </section>
         </>
     );
 };
