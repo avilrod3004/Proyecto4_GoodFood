4c72ba03898f425216f4b62c284f4998c89845f9
avilrod3004 <avilrod3004@g.educaand.es>
2024-12-05 23:01:00 +0100

    maquetacion footer

--- src/layouts/components/Footer.jsx
+++ src/layouts/components/Footer.jsx
@@ -1,14 +1,73 @@
 import React from 'react';
-import "./Footer.css"
+import LogoFacebook from "../../assets/facebook.svg"
+import LogoTwitter from "../../assets/twitter.svg"
+import LogoTikTok from "../../assets/tiktok.svg"
+import LogoInstagram from "../../assets/instagram.svg"
+import LogoGitHub from "../../assets/github.svg"
 
 const Footer = () => {
     return (
-        <footer>
-            <nav>
-                <a href="#">c 2024 GoodFood</a>
-                <a href="#">Terms of Service</a>
-                <a href="#">Privacy Policy</a>
-                <a href="#">Manege Privacy Preferences</a>
+        <footer className="pie">
+            <nav className="pie__menu-cuentas">
+                <ul className="menu-cuentas__listado-cuentas">
+                    <li className="listado-cuentas__opcion-cuentas">
+                        <a href="https://github.com/avilrod3004/Proyecto4_GoodFood" target="_blank"
+                           className="opcion-cuentas__enlace-cuentas">
+                            <img src={LogoGitHub} alt="Logo GitHub" className="enlace-cuentas__logo"/>
+                            <span className="enlace-cuentas__nombre">GitHub</span>
+                        </a>
+                    </li>
+                    <li className="listado-cuentas__opcison-cuentas">
+                        <a href="https://www.facebook.com" target="_blank" className="opcion-cuentas__enlace-cuentas">
+                            <img src={LogoFacebook} alt="Logo Facebook" className="enlace-cuentas__logo"/>
+                            <p className="enlace-cuentas__nombre">Facebook</p>
+                        </a>
+                    </li>
+                    <li className="listado-cuentas__opcion-cuentas">
+                        <a href="https://x.com/" target="_blank" className="opcion-cuentas__enlace-cuentas">
+                            <img src={LogoTwitter} alt="Logo Twitter/X" className="enlace-cuentas__logo"/>
+                            <span className="enlace-cuentas__nombre">X</span>
+                        </a>
+                    </li>
+                    <li className="listado-cuentas__opcion-cuentas">
+                        <a href="https://www.tiktok.com/" target="_blank" className="opcion-cuentas__enlace-cuentas">
+                            <img src={LogoTikTok} alt="Logo TikTok" className="enlace-cuentas__logo"/>
+                            <span className="enlace-cuentas__nombre">TikTok</span>
+                        </a>
+                    </li>
+                    <li className="listado-cuentas__opcion-cuentas">
+                        <a href="https://www.instagram.com/" target="_blank" className="opcion-cuentas__enlace-cuentas">
+                            <img src={LogoInstagram} alt="Logo Instagram" className="enlace-cuentas__logo"/>
+                            <span className="enlace-cuentas__nombre">Instagram</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+            <nav className="pie__menu-terminos">
+                <ul className="menu-terminos__listado-terminos">
+                    <li className="listado-terminos__opcion-terminos">
+                        <a href="#" className="opcion-terminos__enlace-terminos">© 2024 GoodFood</a>
+                    </li>
+                    <li className="listado-terminos__opcion-terminos">
+                        <span className="opcion-terminos__separador">|</span>
+                    </li>
+                    <li className="listado-terminos__opcion-terminos">
+                        <a href="#" className="opcion-terminos__enlace-terminos">Terms of Service</a>
+                    </li>
+                    <li className="listado-terminos__opcion-terminos">
+                        <span className="opcion-terminos__separador">|</span>
+                    </li>
+                    <li className="listado-terminos__opcion-terminos">
+                        <a href="#" className="opcion-terminos__enlace-terminos">Privacy Policy</a>
+                    </li>
+                    <li className="listado-terminos__opcion-terminos">
+                        <span className="opcion-terminos__separador">|</span>
+                    </li>
+                    <li className="listado-terminos__opcion-terminos">
+                        <a href="#" className="opcion-terminos__enlace-terminos">Manege Privacy Preferences</a>
+                    </li>
+                </ul>
             </nav>
         </footer>
     );
