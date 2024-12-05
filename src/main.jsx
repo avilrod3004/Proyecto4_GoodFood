6fca76ca70ad457ecbb874b39aac3c8628286a92
avilrod3004 <avilrod3004@g.educaand.es>
2024-12-05 19:33:49 +0100

    importar main de scss

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import {RouterProvider} from "react-router-dom";
 import {router} from "./router/index.jsx";
 import UserProvider from "./context/UserContext.jsx";
+import './sass/main.scss';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
