9b99468bc99c01c20ffabe577ab5894d31afaddc
avilrod3004 <avilrod3004@g.educaand.es>
2024-12-06 20:54:59 +0100

    loading: gamba

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,15 +3,17 @@ import Carrot from "../assets/carrot.svg";
 import Fish from "../assets/fish.svg";
 import Apple from "../assets/apple.svg";
 import Chicken from "../assets/chicken.svg";
+import Shrimp from "../assets/shrimp.svg";
 
 const Loading = () => {
     return (
-        <div className="loading">
-            <img className="loading__imagen" src={Carrot} alt=""/>
-            <img className="loading__imagen" src={Fish} alt=""/>
-            <img className="loading__imagen" src={Apple} alt=""/>
-            <img className="loading__imagen" src={Chicken} alt=""/>
-        </div>
+        <main className="loading">
+            <img className="loading__imagen" src={Carrot} alt="carrot"/>
+            <img className="loading__imagen" src={Fish} alt="fish"/>
+            <img className="loading__imagen" src={Apple} alt="apple"/>
+            <img className="loading__imagen" src={Chicken} alt="chicken"/>
+            <img className="loading__imagen" src={Shrimp} alt="shrimp"/>
+        </main>
     );
 };
 
