54563a19fa943479c398f5e0f29f3f8184bb0914
avilrod3004 <avilrod3004@g.educaand.es>
2024-12-06 21:26:40 +0100

    aplicar filtros predefinidos dependiendo de la pagina

--- src/pages/Breakfast.jsx
+++ src/pages/Breakfast.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import SearchRecipes from "../components/SearchRecipes.jsx";
 
 const Breakfast = () => {
+    // Estado inicial
+    const filtersInitialValues = {
+        q: "",
+        mealTypes: ["Breakfast"],
+        cuisineTypes: [],
+        health: [],
+        maxTime: "" // Aplicar filtro por defecto
+    }
+
     return (
         <>
-            Breakfast
+            <SearchRecipes filtersInitialValues={filtersInitialValues} page="mealsTypes"/>
         </>
     );
 };
--- src/pages/LunchDinner.jsx
+++ src/pages/LunchDinner.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import SearchRecipes from "../components/SearchRecipes.jsx";
 
 const LunchDinner = () => {
+    // Estado inicial
+    const filtersInitialValues = {
+        q: "",
+        mealTypes: ["Dinner", "Lunch"],
+        cuisineTypes: [],
+        health: [],
+        maxTime: "" // Aplicar filtro por defecto
+    }
+
     return (
         <>
-            Lunch/dinner
+            <SearchRecipes filtersInitialValues={filtersInitialValues} page="mealsTypes"/>
         </>
     );
 };
