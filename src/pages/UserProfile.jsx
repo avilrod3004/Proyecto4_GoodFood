89bfd6db2695be6dbbe842a90f8cff89512c6be9
avilrod3004 <avilrod3004@g.educaand.es>
2024-12-07 23:00:03 +0100

    maquetacion: perfil del usuario

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -4,6 +4,16 @@ import SmallCard from "../components/SmallCard.jsx";
 import BigCard from "../components/BigCard.jsx";
 import Loading from "../components/Loading.jsx";
 
+import LinkLight from "../assets/link_light.svg"
+import LinkDark from "../assets/link_dark.svg"
+import BigCardDark from "../../src/assets/big_cards_dark.svg";
+import BigCardLight from "../assets/big_cards_light.svg";
+import SmallCardDark from "../../src/assets/small_cards_dark.svg";
+import SmallCardLight from "../assets/small_cards_light.svg";
+import UserDefault from "../assets/user_profile_default.jpg"
+import SadFaceLight from "../assets/sad_face_light.svg";
+import SadFaceDark from "../../src/assets/sad_face_dark.svg";
+
 const UserProfile = () => {
     // Estados iniciales
     const userDataInitial = {
@@ -51,43 +61,101 @@ const UserProfile = () => {
     return (
         <main className="perfil">
             <aside className="perfil__datos">
-                <h1 className="datos__nombre-usuario">{userData.userName || "No hay usuario"}</h1>
-                <img className="datos__foto" src={userData.picture || ""} alt="Foto de perfil"/>
-                <p className="datos__biografia">{userData.biography || "No hay biografía"}</p>
+                <h1 className="datos__nombre-usuario">{userData.userName || "User name"}</h1>
+                <img className="datos__foto" src={userData.picture || UserDefault} alt="Profile picture"/>
+                <p className="datos__biografia">{userData.biography || "Hello!"}</p>
 
-                <h2 className="datos__web">Website</h2>
-                <p>
-                    <img src="" alt=""/>
-                    <a href={userData.website || "#"} target="_blank">
-                        {userData.website || "No hay website"}
-                    </a>
-                </p>
+                {
+                    userData.website !== "" && (
+                        <>
+                            <h2 className="datos__titulo">Website</h2>
+                            <p className="datos__website">
+                                <img className="website__icono" src={LinkLight} alt="Link"/>
+                                <a className="website__enlace" href={userData.website} target="_blank">
+                                    {userData.website}
+                                </a>
+                            </p>
+                        </>
+                    )
+                }
 
-                <h2>Social accounts</h2>
-                <ul>
-                    <li>
-                        <img src="" alt=""/>
-                        <a href="">{userData.socialAccount1 || " No hay social account"}</a>
-                    </li>
-                    <li>
-                        <img src="" alt=""/>
-                        <a href="">{userData.socialAccount2 || " No hay social account"}</a>
-                    </li>
-                    <li>
-                        <img src="" alt=""/>
-                        <a href="">{userData.socialAccount3 || " No hay social account"}</a>
-                    </li>
-                </ul>
+                {
+                    userData.socialAccount1 !== "" || userData.socialAccount2 !== "" || userData.socialAccount3 !== "" && (
+                        <>
+                            <h2 className="datos__titulo">Social accounts</h2>
+                            <ul className="datos__listado-redes">
+                                {
+                                    userData.socialAccount1 !== "" && (
+                                        <li className="listado-redes__cuenta">
+                                            <img className="cuenta__icono" src={LinkLight} alt="Link"/>
+                                            <a className="cuenta__enlace"
+                                               href="">{userData.socialAccount1 || " No hay social account"}</a>
+                                        </li>
+                                    )
+                                }
+                                {
+                                    userData.socialAccount2 !== "" && (
+                                        <li className="listado-redes__cuenta">
+                                            <img className="cuenta__icono" src={LinkLight} alt="Link"/>
+                                            <a className="cuenta__enlace"
+                                               href="">{userData.socialAccount2 || " No hay social account"}</a>
+                                        </li>
+                                    )
+                                }
+                                {
+                                    userData.socialAccount3 !== "" && (
+                                        <li className="listado-redes__cuenta">
+                                            <img className="cuenta__icono" src={LinkLight} alt="Link"/>
+                                            <a className="cuenta__enlace"
+                                               href="">{userData.socialAccount3 || " No hay social account"}</a>
+                                        </li>
+                                    )
+                                }
+                            </ul>
+                        </>
+                    )
+                }
             </aside>
 
-            <section className="perfil__recetas">
-                <h1>My favorite recipes</h1>
-                <header>
-                    <button onClick={() => {
-                        cardSize === "small" ? setCardSize("big") : setCardSize("small")
-                    }}>Change card size
-                    </button>
+            <section className="perfil__recetas-perfil">
+                <header className="recetas-perfil__encabezado">
+                    <h1 className="encabezado__titulo">My favorite recipes</h1>
+
+                    <a
+                        className="encabezado__vista"
+                        onClick={() => {
+                            cardSize === "small" ? setCardSize("big") : setCardSize("small")
+                        }}
+                    >
+                        {
+                            cardSize === "small"
+                                ? (
+                                    <img
+                                        className="vista__imagen"
+                                        src={BigCardLight}
+                                        alt="Change cards size"
+                                    />
+                                ) : (
+                                    <img
+                                        className="vista__imagen"
+                                        src={SmallCardLight}
+                                        alt="Change cards size"
+                                    />
+                                )
+                        }
+                    </a>
                 </header>
+
+                {
+                    userData.favoriteRecipes.length === 0 && (
+                        <div className="recetas-perfil__nada-perfil">
+                            <img className="nada-perfil__imagen" src={SadFaceLight} alt=":("/>
+                            <p className="nada-perfil__mensaje">
+                                You don&#39;t have any recipes saved as favorites yet.
+                            </p>
+                        </div>
+                    )
+                }
 
                 {
                     userData.favoriteRecipes.map((recipe) => (
