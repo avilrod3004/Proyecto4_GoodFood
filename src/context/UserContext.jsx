5764647465c3c79a93d3411a8cc58d9075aeebc5
avilrod3004 <avilrod3004@g.educaand.es>
2024-12-08 16:21:04 +0100

    correccion loading

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,6 +1,7 @@
 import {createContext, useEffect, useState} from "react";
 import { onAuthStateChanged } from 'firebase/auth'
 import {auth} from "../config/Firebase.jsx";
+import Loading from "../components/Loading.jsx";
 
 export const UserContext = createContext();
 
@@ -17,7 +18,7 @@ const UserProvider = ({ children }) => {
     }, [])
 
     if (user === null) {
-        return <p>Loading...</p>;
+        return <Loading/>;
     }
 
     return (
