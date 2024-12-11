3c2673523a2aa5dd9304202dde992fd84aef3d96
avilrod3004 <avilrod3004@g.educaand.es>
2024-12-11 18:32:55 +0100

    correccion main loading...

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,11 @@ import {router} from "./router/index.jsx";
 import UserProvider from "./context/UserContext.jsx";
 import './sass/main.scss';
 import ThemeProvider from "./context/ThemeContext.jsx";
+import Loading from "./components/Loading.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loading/>}>
         <ThemeProvider>
             <UserProvider>
                 <RouterProvider router={router}/>
