888a4947cf0fac31717ba636eb951c257919976d
avilrod3004 <avilrod3004@g.educaand.es>
2024-12-12 22:40:56 +0100

    aplicar css externo

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -13,13 +13,37 @@ const NotFound = () => {
     const error = useRouteError();
     const {theme, toggleTheme} = useContext(ThemeContext);
 
+    // 3.2 Debemos añadir dos formas de incluir estilos CSS de forma directa y explicar si es recomendable su uso.
+    /*
+    * Los estilos en línea hace que sea dificil mantener del código y
+    * el CSS incrustado solo afecta al documento donde se agrega, lo que limita reutilizar los estilos
+    * En conclusión, lo recomendable es definir los estilos en un fichero css externo
+    * */
+
     return (
-        <main className="not-found">
-            <img className="not-found__imagen" src={theme === "light" ? errorImageLight : errorImageDark} alt="Error 404"/>
-            <h1 className="not-found__titulo">Page not found</h1>
-            <p className="not-found__mensaje">The page you&#39;re looking for doesn&#39;t exist or has been moved</p>
-            <Link className="not-found__enlace" to="/">Back to Home</Link>
-        </main>
+        <>
+            <style>
+                {/*CSS incrustado*/}
+                {`
+                    .not-found-2 {
+                      display: flex;
+                      flex-direction: column;
+                      align-items: center;
+                      justify-content: center;
+                      gap: 1rem;
+                    }
+                `}
+            </style>
+            <main className="not-found not-found-2">
+                <img className="not-found__imagen" src={theme === "light" ? errorImageLight : errorImageDark}
+                     alt="Error 404"/>
+                <h1 className="not-found__titulo">Page not found</h1>
+                {/*Estilos en línea */}
+                <p className="not-found__mensaje" style={{textAlign: "center"}}>The page you&#39;re looking for
+                    doesn&#39;t exist or has been moved</p>
+                <Link className="not-found__enlace" to="/">Back to Home</Link>
+            </main>
+        </>
     );
 };
 
